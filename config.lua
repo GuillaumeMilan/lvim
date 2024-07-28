77ee92a92a06b4ed056dacb30756c949aee385dc
Guillaume Milan <guillaume.milan@sonio.ai>
2024-07-28 23:51:32 +0200

    Initial commit

---
+++ config.lua
@@ -0,0 +1,51 @@
+-- Default plugin
+lvim.builtin.cmp.completion.keyword_length = 5
+lvim.builtin.telescope.defaults.layout_config.width = 0.95
+lvim.builtin.telescope.defaults.layout_config.preview_cutoff = 75
+
+require "user.copilot"
+require "user.aliases"
+
+-- User plugings
+lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  --{ "github/copilot.vim" },
+}
+
+lvim.colorscheme =  "aurora"
+
+-- lvim.format_on_save = true
+
+lvim.builtin.treesitter.ensure_installed = {
+  "javascript",
+  "json",
+  "lua",
+  "typescript",
+  "tsx",
+  "css",
+  "elixir",
+  "heex",
+}
+
+lvim.lsp.installer.setup.ensure_installed = {
+  "lua_ls",
+  "cssls",
+  "tsserver",
+  "tailwindcss",
+  "elixirls"
+}
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tailwindcss" })
+vim.opt.mouse = ""
+
+local opts = {
+  root_dir = function(fname)
+    local util = require "lspconfig/util"
+    return util.root_pattern("assets/tailwind.config.js", "tailwind.config.js", "tailwind.config.cjs", "tailwind.js",
+      "tailwind.cjs")(fname)
+  end,
+  init_options = {
+    userLanguages = { heex = "html", elixir = "html" }
+  },
+}
+require("lvim.lsp.manager").setup("tailwindcss", opts)
---
+++ lua/user/aliases.lua
@@ -0,0 +1,47 @@
+-- Unassign bindings
+lvim.builtin.which_key.mappings["c"] = {}
+
+-- Keybindings
+lvim.builtin.which_key.mappings["b"]["d"] = { "<cmd>BufferKill<CR>", "Close" }
+lvim.builtin.which_key.mappings["b"]["g"] = {
+  name = "GoTo",
+  ["1"] = { "<cmd>BufferLineGoTo 1<CR>", "1" },
+  ["2"] = { "<cmd>BufferLineGoTo 2<CR>", "2" },
+  ["3"] = { "<cmd>BufferLineGoTo 3<CR>", "3" },
+  ["4"] = { "<cmd>BufferLineGoTo 4<CR>", "4" },
+  ["5"] = { "<cmd>BufferLineGoTo 5<CR>", "5" },
+  ["6"] = { "<cmd>BufferLineGoTo 6<CR>", "6" },
+  ["7"] = { "<cmd>BufferLineGoTo 7<CR>", "7" },
+  ["8"] = { "<cmd>BufferLineGoTo 8<CR>", "8" },
+  ["9"] = { "<cmd>BufferLineGoTo 9<CR>", "9" },
+  ["0"] = { "<cmd>BufferLineGoTo 10<CR>", "10" },
+}
+
+
+lvim.builtin.which_key.mappings["l"]["g"] = {
+  name = "Helper",
+  ["k"] = { "<cmd>lua vim.lsp.buf.hover()<cr><cmd>lua vim.lsp.buf.hover()<cr>", "Show hover" },
+  ["d"] = { "<cmd>lua vim.lsp.buf.definition()<cr>", "definition" },
+  ["D"] = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "Declaration" },
+  ["r"] = { "<cmd>lua vim.lsp.buf.references()<cr>", "references" },
+  ["I"] = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "Implementation" },
+  ["s"] = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "show signature help" },
+  ["l"] = {
+    function()
+      local float = vim.diagnostic.config().float
+
+      if float then
+        local config = type(float) == "table" and float or {}
+        config.scope = "line"
+
+        vim.diagnostic.open_float(config)
+      end
+    end,
+    "Show line diagnostics",
+  },
+}
+
+lvim.builtin.which_key.mappings["u"] = {
+  name = "Utils",
+  ["h"] = { '<cmd>let @/=""<CR>', "No Highlight" },
+}
